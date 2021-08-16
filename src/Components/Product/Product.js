960685619bd0b469a42b7d39e3d847abeceda4ff
new name <new email>
2021-08-16 16:06:01 +0600

    second

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React from "react";
+import "./Product.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCartPlus } from "@fortawesome/free-solid-svg-icons";
 
-const Product = () => {
-    return (
-        <div>
-            <h3>This is product</h3>
-        </div>
-    );
+const Product = (props) => {
+  const cartIcon = <FontAwesomeIcon icon={faCartPlus} />;
+  const { img, name, seller, price, stock } = props.product;
+  return (
+    <div className="product">
+      <div>
+        <img src={img} alt="" />
+      </div>
+      <div className="product-name">
+        <h4>{name}</h4>
+        <br />
+        <p>
+          <small>by: {seller}</small>
+        </p>
+        <p>${price}</p>
+        <br />
+        <p>
+          <small> Only {stock} left in stock</small>
+        </p>
+        <button className="main-button" onClick={props.handleAddProduct}> {cartIcon} add to cart</button>
+      </div>
+    </div>
+  );
 };
 
 export default Product;
--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -1,19 +1,22 @@
 import React, { useState } from "react";
 import fakeData from "../../fakeData";
+import Product from "../Product/Product";
 import './Shop.css'
 
 const Shop = () => {
   const first10 = fakeData.slice(0, 10);
   const [products, setProducts] = useState(first10);
 
+  const handleAddProduct = () => {
+    console.log('product added')
+  }
+
   return (
     <div className="shop-container">
       <div className="product-container">
-        <ul>
           {products.map((product) => (
-            <li>{product.name}</li>
+            <Product product={product} handleAddProduct={handleAddProduct}/>
           ))}
-        </ul>
       </div>
       <div className="cart-container">
           <h3>This is cart</h3>
